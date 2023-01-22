6322f6e6d6e7bdb4b5e5b6f264a21a41481b99d2
Adel Mohamadzade <adel.mohamadzadeph@gmail.com>
2023-01-22 21:56:25 +0330

    Setup project.

---
+++ src/account/models.py
@@ -0,0 +1,6 @@
+from sqlalchemy import (
+    Column, ForeignKey, Integer, String, Boolean
+)
+from sqlalchemy.orm import relationship
+
+from db.database import Base
---
+++ src/db/database.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./fastapi-practice.db'
+
+engin = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
+)
+session_local = sessionmaker(autocommit=False, autoflush=False, bind=engin)
+
+Base = declarative_base()
+
+def get_db():
+    db = session_local()
+    try:
+        yield db
+    finally:
+        db.close()
